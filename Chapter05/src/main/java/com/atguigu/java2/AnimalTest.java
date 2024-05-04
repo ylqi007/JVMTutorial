983e04529c4172ba7605917e4566c205201e77c7
ylqi007 <ylqi92121@gmail.com>
2024-05-04 12:15:56 -0700

    Chapter05_6, 方法调用: 解析与分派

---
+++ Chapter05/src/main/java/com/atguigu/java2/AnimalTest.java
@@ -0,0 +1,64 @@
+package com.atguigu.java2;
+
+/**
+ * 说明早期绑定和晚期绑定的例子
+ * @author shkstart
+ * @create 2020 上午 11:59
+ */
+public class AnimalTest {
+    public void showAnimal(Animal animal){  // Animal为父类
+        animal.eat();   //表现为：晚期绑定
+    }
+    public void showHunt(Huntable h){   // Huntable为接口
+        h.hunt();   //表现为：晚期绑定
+    }
+}
+
+
+class Animal{
+
+    public void eat(){
+        System.out.println("动物进食");
+    }
+}
+
+
+interface Huntable {
+    void hunt();
+}
+
+
+class Dog extends Animal implements Huntable {
+    @Override
+    public void eat() {
+        System.out.println("狗吃骨头");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println("捕食耗子，多管闲事");
+    }
+}
+
+
+class Cat extends Animal implements Huntable {
+
+    public Cat(){
+        super();//表现为：早期绑定
+    }
+
+    public Cat(String name){
+        this();//表现为：早期绑定
+    }
+
+    @Override
+    public void eat() {
+        super.eat();//表现为：早期绑定
+        System.out.println("猫吃鱼");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println("捕食耗子，天经地义");
+    }
+}
