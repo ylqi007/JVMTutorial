fa1d81d4350151350ada193223c8306339dde215
ylqi007 <ylqi92121@gmail.com>
2024-05-22 16:14:11 -0700

    Chapter15, 标记阶段: 引用计数算法, 可达性分析算法

---
+++ Chapter15/src/main/java/com/atguigu/Main.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * Description:
+ *
+ * @Author: ylqi007
+ * @Create: 5/22/24 14:42
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
