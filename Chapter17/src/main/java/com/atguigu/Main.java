02d69d5f3de5617142cd6f09bb9302f7b7595df8
ylqi007 <ylqi92121@gmail.com>
2024-06-26 09:28:39 -0700

    Chapter17_1, 垃圾回收器

---
+++ Chapter17/src/main/java/com/atguigu/Main.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * Description:
+ *
+ * @Author: ylqi007
+ * @Create: 6/26/24 09:21
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
