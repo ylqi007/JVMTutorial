5614ce9943c6d78c31bd7dd9c92b0fa84e5f131f
ylqi007 <ylqi92121@gmail.com>
2025-09-06 10:16:47 -0700

    复习Chap10 对象的实例化内存布局与访问定位

--- JVMPart1/src/main/java/com/ylqi007/chap10createinstance/Demo01ObjectTest.java
+++ JVMPart1/src/main/java/com/ylqi007/chap10createinstance/Demo01ObjectTest.java
@@ -1,10 +1,10 @@
-package com.atguigui.java;
+package com.ylqi007.chap10createinstance;
 
 /**
  * @author shkstart  shkstart@126.com
  * @create 2020  17:16
  */
-public class ObjectTest {
+public class Demo01ObjectTest {
     public static void main(String[] args) {
         Object obj = new Object();
     }
--- JVMPart1/src/main/java/com/ylqi007/chap10createinstance/Demo02CustomerTest.java
+++ JVMPart1/src/main/java/com/ylqi007/chap10createinstance/Demo02CustomerTest.java
@@ -1,10 +1,10 @@
-package com.atguigui.java;
+package com.ylqi007.chap10createinstance;
 
 /**
  * @author shkstart  shkstart@126.com
  * @create 2020  18:42
  */
-public class CustomerTest {
+public class Demo02CustomerTest {
     public static void main(String[] args) {
         Customer cust = new Customer();
     }
